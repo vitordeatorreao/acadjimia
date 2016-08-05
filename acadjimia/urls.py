9451627ec51e4091c61efeae661a636989ead36f
vitordeatorreao <vitordeatorreao@gmail.com>
2016-08-05 11:42:02 -0300

    Adicionando página de quem somos

--- acadjimia/urls.py
+++ acadjimia/urls.py
@@ -19,11 +19,13 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth.views import logout
+from django.views.generic import TemplateView
 
 from core.views import IndexView, login_or_redirect, RegisterUser
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^sobre/$', TemplateView.as_view(template_name='aboutus.html'), name='aboutus'),
     url(r'^entrar/$', login_or_redirect, {'template_name': 'login.html'}, name="login"),
     url(r'^sair/$', logout, {'next_page': 'index'}, name='logout'),
     url(r'^criar-conta/$', RegisterUser.as_view(), name='register'),
